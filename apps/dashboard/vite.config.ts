23eca38d8e2859434784ea84867a919741f44141
Gustavo B <gugabit2810@gmail.com>
2026-07-26 19:56:05 -0300

    chore: remove unecessary changes

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@telivityhaip/shared': path.resolve(__dirname, '../../packages/shared/src/index.ts'),
     },
   },
   server: {
